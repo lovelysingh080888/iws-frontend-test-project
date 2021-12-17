88bbae27b0629b324b05f52664248d548b04fee2
SAASNA <mayankawasthi88@gmail.com>
2021-12-17 15:57:37 +0530

    first commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,34 +2,41 @@ import React, { useState, useEffect } from "react";
 import { Header } from "./Partials/Header";
 import axios from "axios";
 import "rsuite/dist/rsuite.min.css";
-import { Data } from "./Action";
+
 export const Dashboard = () => {
+  const [filter,setFilter] = useState({preset:null,from:null,to:null});
   let [holidays, setHolidays] = useState([]);
+  const d = new Date();
+  const date =d.getFullYear()+"-"+d.getMonth+"-"+d.getDate()
+  
   const getData = async () => {
     await axios
       .get("https://www.gov.uk/bank-holidays.json")
       .then((resp) => {
         const dataCollect = resp.data;
         const keys = Object.keys(resp.data);
-
         keys.forEach((item) => {
           dataCollect[item]["events"].forEach((elem) => {
-            holidays.push(JSON.stringify(elem));
+            holidays.push(elem);
             setHolidays(holidays);
-            console.log(elem);
+            console.log(elem)
           });
         });
       })
       .catch((err) => console.log(err));
   };
-
-  useEffect(() => {
-    getData();
+ const setFilterData = (data)=>{
+   setFilter(data)
+ }
+  useEffect(async() => {
+   await getData();
+ 
   }, []);
 
   return (
     <>
       <Header />
+      
       <div className="main-container">
         <div className="main-heading">
           <div className="row">
@@ -41,11 +48,12 @@ export const Dashboard = () => {
                 <div className="row">
                   <div className="col-sm-3">
                     <label>Preset</label>
-                    <select className="form-control">
+                    <select className="form-control" onChange={(e)=>setFilterData({preset:e.target.value})}>
                       <option value="">-Select-</option>
-                      <option value="">Today</option>
-                      <option value="">Yesterday</option>
-                      <option value="">Last Week</option>
+                      <option value={date}>Today</option>
+                      <option value="-1">Yesterday</option>
+                      <option value="-7">Last Week</option>
+                      <option value="-30">Last Month</option>
                     </select>
                   </div>
                   <div className="pt-4">
@@ -55,11 +63,11 @@ export const Dashboard = () => {
                   </div>
                   <div className="col-sm-3">
                     <label>From</label>
-                    <input type="date" className="form-control" />
+                    <input type="date" className="form-control" onChange={(e)=>setFilterData({from:e.target.value})} />
                   </div>
                   <div className="col-sm-3">
                     <label>To</label>
-                    <input type="date" className="form-control" />
+                    <input type="date" className="form-control" onChange={(e)=>setFilterData({to:e.target.value})} />
                   </div>
                   <div className="col-sm-2 pt-3">
                     <a href="#">Filter Data</a>
@@ -72,8 +80,8 @@ export const Dashboard = () => {
 
         <div className="customer-page">
           <table
-            id="example"
-            className="dataTables_main table table-striped table-responsive"
+            
+            className=" table table-striped table-responsive"
           >
             <thead>
               <tr>
